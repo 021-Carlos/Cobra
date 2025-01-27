5678ca88fde944a401f87a028a57fd37d5d67e46
Carlos <c.silvax2008@gmail.com>
2025-01-27 17:46:00 -0300

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,8 @@
+const cElemento = document.querySelector("canvas");
+const cCtx = cElemento.getContext("2d");
+
+function setup(){
+    cElemento.width = cCtx.width = screen.width;
+    cElemento.height = cCtx.height = screen.width;
+}
+setup();
