8e6fc946d7b8a0e7c3b6f846239ce6c9c34e4f00
Carlos <c.silvax2008@gmail.com>
2025-01-27 17:44:05 -0300

    Update tela.js

--- tela.js
+++ tela.js
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jogo da cobra</title>
-</head>
-<body>
-    <canvas></canvas>
-    <script src= canvas.js> </script>
-    <script src= tela.js> </script>
-    <script src= placar.js> </script>
-    <script src= cobra.js> </script>
-</body>
-</html>
+const tela= {
+    largura : 800, 
+    altura : 400,
+    cor : "green",
+    desenhar(){
+        cCtx.fillStyle=this.cor;
+        cCtx.fillRect(0,60,this.largura,this.altura)
+    }
+}
+tela.desenhar();
