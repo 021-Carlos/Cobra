aa92c0137cddcd5c60ff926f63c868c1454496b2
Carlos <c.silvax2008@gmail.com>
2025-02-03 17:20:17 -0300

    Update cobra.js

--- cobra.js
+++ cobra.js
@@ -4,9 +4,38 @@ const cobra = {
     tamanho:10,
     largura:10,
     cor:"red",
-    desenhar(){
+    comprimento:1,
+    x:400,
+    y:260,
+    tamanho:10,
+    largura:10,
+    direcao:'r',
+    vida:3,
+    desenhar() {
         cCtx.fillStyle=this.cor;
         cCtx.fillRect(this.x,this.y,this.tamanho,this.largura);
+    },
+
+    mover(){
+        if (this.direcao =='r')
+            this.x++;
+        if (this.direcao == 'l')
+            this.x--;
+        if (this.direcao == 'd')
+            this.y++;
+        if (this.direcao == 'u')
+            this.y--
+        if (this.x < 0 || this.x > (tela.largura-this.tamanho))
+            this.morrer();
+        if  (this.x < 60 || this.y > (tela.altura+placar.altura-this.tamanho))    
+            this.morrer();
+
+    },
+
+    morrer(){
+        this.vida--;
+        this.x=400;
+        this.y=260;
+
     }
 }
-cobra.desenhar();
