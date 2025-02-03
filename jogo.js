91cdd83858c8968f786ed3e065ae052278aa7c7b
Carlos <c.silvax2008@gmail.com>
2025-02-03 17:28:10 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,18 +1,19 @@
-function play(){
-        tela.desenhar();
-        cobra.desenhar();
-        placar.desenhar();
-        cobra.mover();
-        comida.desenhar();
-        if (cobra.vida > 0)
-            requestAnimationFrame(play);
+class Comida {
+    tamanho=15;
+    imagem;
+    imagemArq='alimento.png';
+    constructor (x,y){
+        this.x = x;
+        this.y = y;
+        this.imagem = new Image();
+        this.imagem.src=this.imagemArq;
+    
+    };
+    desenhar(){
+        cCtx.drawImage(this.imagem,
+                        this.x,
+                        this.y,
+                        this.tamanho,
+                        this.tamanho)
+    }
 }
-const comida = new Comida(300,300)
-play();
-document.addEventListener("keydown",(e) => {
-    if (e.key == 8) cobra.direcao='u';
-    if (e.key == 6) cobra.direcao='r';
-    if (e.key == 2) cobra.direcao='d';
-    if (e.key == 4) cobra.direcao='l';
-
-})
